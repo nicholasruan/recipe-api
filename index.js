7ab035f59f90a8c558316cbf3a2e5cc51df2466c
Nicholas Ruan <nicholas.ruan.nr@gmail.com>
2019-09-06 15:14:58 -0400

    fix

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', (request, response) => {
 })
 
 app.get('/recipes', db.getRecipes);
-app.post('/create', db.createRecipe)
+app.post('/create', db.createRecipe);
 
 app.listen(process.env.PORT || 3002, () => {
   console.log(`Server listening ${process.env.PORT}`)
--- queries.js
+++ queries.js
@@ -21,6 +21,7 @@ const createRecipe = (request, response) => {
 		response.status(500).json({ 'error' : 'MATE! Key value is incorrect...'})
 	} else {
 		const { name, category, ingredients } = request.body;
+		console.log(request.body);
 		pool.query('INSERT INTO recipes (name, category, ingredients) VALUES ($1, $2, $3)', [name, category, ingredients], (error, results) => {
     if (error) {
 			console.log(error);
