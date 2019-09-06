f2c126e4ce0a742f9d1c77851c768f4b5381a21c
Nicholas Ruan <nicholas.ruan.nr@gmail.com>
2019-09-06 14:09:45 -0400

    change header

--- queries.js
+++ queries.js
@@ -4,17 +4,16 @@ const { pool } = require('./config')
 const API_KEY = 'miloislife';
 
 const getRecipes = (request, response) => {
-	// console.log(request.headers.key);
-	// if (request.headers.key !== API_KEY) {
-	// 	response.status(500).json({ 'error' : 'MATE! Key value is incorrect...'})
-	// } else {
+	if (request.headers.key !== API_KEY) {
+		response.status(500).json({ 'error' : 'MATE! Key value is incorrect...'})
+	} else {
 		pool.query('SELECT * FROM recipes ORDER BY id ASC', (error, results) => {
 			if (error) {
 				throw error
 			}
 			response.status(200).json(results.rows)
 		})
-	// }
+	}
 
 }
 
