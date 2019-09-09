8412964d9b03052d963da13bb4340251aa2dbd0d
Nicholas Ruan <nicholas.ruan.nr@gmail.com>
2019-09-08 23:38:21 -0400

    added image upload

---
+++ imageupload.js
@@ -0,0 +1,66 @@
+// route/api/profile.js
+const express = require( 'express' );
+const aws = require( 'aws-sdk' );
+const multerS3 = require( 'multer-s3' );
+const multer = require('multer');
+const path = require( 'path' );
+const url = require('url');
+
+const router = express.Router();
+
+const s3 = new aws.S3({
+ accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+ secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+ Bucket: process.env.S3_BUCKET_NAME,
+});
+
+const profileImgUpload = multer({
+ storage: multerS3({
+  s3: s3,
+  bucket: process.env.S3_BUCKET_NAME,
+  acl: 'public-read',
+  key: function (req, file, cb) {
+   cb(null, path.basename( file.originalname, path.extname( file.originalname ) ) + '-' + Date.now() + path.extname( file.originalname ) )
+  }
+ }),
+ limits:{ fileSize: 2000000 }, // In bytes: 2000000 bytes = 2 MB
+ fileFilter: function( req, file, cb ){
+  checkFileType( file, cb );
+ }
+}).single('profileImage');
+
+function checkFileType( file, cb ){
+ const filetypes = /jpeg|jpg|png|gif/;
+ const extname = filetypes.test( path.extname( file.originalname ).toLowerCase());
+ const mimetype = filetypes.test( file.mimetype );
+if( mimetype && extname ){
+  return cb( null, true );
+ } else {
+  cb( 'Error: Images Only!' );
+ }
+}
+
+router.post( '/img-upload', ( req, res ) => {
+		profileImgUpload( req, res, ( error ) => {
+		  if( error ){
+		   console.log( 'errors', error );
+		   res.json( { error: error } );
+		  } else {
+		   if( req.file === undefined ){
+		    console.log( 'Error: No File Selected!' );
+		    res.json( 'Error: No File Selected' );
+		   } else {
+		    const imageName = req.file.key;
+		    const imageLocation = req.file.location;
+
+				res.status(200).json( {
+		    	image: imageName,
+		    	location: imageLocation
+		    });
+
+		   }
+		  }
+		 });
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-var cors = require("cors");
+const cors = require("cors");
 const app = express();
 const db = require('./queries');
 const aws = require('aws-sdk');
+const fs = require('fs');
 
+app.set('views', './views');
+app.engine('html', require('ejs').renderFile);
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+// render display page
+app.get('/imguploadtest', (req, res) => res.render('imgupload.html'));
 
 // API Endpoints
 app.get('/api', (request, response) => {
   response.json({ info: 'Hello boss, this is the recipe API. Enjoy the feast!' })
 });
 
+const profile = require( './imageupload' );
+app.use('/api', profile );
 app.get('/api/recipes', db.getRecipes);
 app.post('/api/create', db.createRecipe);
 app.delete('/api/delete', db.deleteRecipe);
@@ -26,9 +29,14 @@ app.get('/api/searchName', db.searchName);
 app.get('/api/searchCategory', db.searchCategory);
 
 app.listen(process.env.PORT || 3002, () => {
-  console.log(`Server listening ${process.env.PORT}`)
+	if (process.env.PORT) {
+		console.log(`Server listening ${process.env.PORT}`);
+	} else {
+		console.log(`Server listening 3002`)
+	}
 });
 
-aws.config.region = 'us-east-1';
-
-const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
+app.post('/api/imageUpload', (request, response) => {
+	console.log(request.body);
+	 response.send(request.body);
+});
